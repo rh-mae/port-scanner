8ba4532c51b89727588a200ccfd1add8068d5d1d
Dave <rh.davitkhachatryan@gmail.com>
2025-04-06 00:28:11 +0400

    added Argument Structure

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,19 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about = "🛡 Rust Port Scanner")]
+struct Args {
+    /// IP-адрес, который нужно просканировать
+    #[arg(short, long)]
+    ip: IpAddr,
+
+    /// Начальный порт
+    #[arg(short, long, default_value_t = 1)]
+    start_port: u16,
+
+    /// Конечный порт
+    #[arg(short, long, default_value_t = 1024)]
+    end_port: u16,
+}
 
