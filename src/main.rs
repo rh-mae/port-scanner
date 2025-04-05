2eba130cdc07170b63be1af2e7e82b0920e1e245
Dave <rh.davitkhachatryan@gmail.com>
2025-04-06 00:28:58 +0400

    added features to main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ use tokio::net::TcpStream;
 use std::net::IpAddr;
 use std::time::Duration;
 
-fn main() {
-    println!("Hello, world!");
-}
-
 #[derive(Parser, Debug)]
 #[command(author, version, about = "🛡 Rust Port Scanner")]
 struct Args {
@@ -23,3 +19,32 @@ struct Args {
     end_port: u16,
 }
 
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    println!(
+        "🔍 Сканирую {} с порта {} по {}",
+        args.ip, args.start_port, args.end_port
+    );
+
+    let mut handles = vec![];
+
+    for port in args.start_port..=args.end_port {
+        let ip = args.ip;
+        handles.push(tokio::spawn(async move {
+            let addr = format!("{}:{}", ip, port);
+            let timeout = Duration::from_millis(500);
+
+            if let Ok(Ok(_)) = tokio::time::timeout(timeout, TcpStream::connect(&addr)).await {
+                println!("✅ Порт {} открыт", port);
+            }
+        }));
+    }
+
+    for handle in handles {
+        let _ = handle.await;
+    }
+
+    println!("✅ Готово!");
+}
